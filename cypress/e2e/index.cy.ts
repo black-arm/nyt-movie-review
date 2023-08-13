88954fd1915b24b0b3d82e6bf4c5af08a0f0f466
Antonio Basile <antoniobasile2@eng.it>
2023-08-13 10:33:17 +0200

    MR-19 test: close modal test

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -10,9 +10,6 @@ describe('index.tsx', () => {
         cy.getByData('query').find('input').type('Super Mario')
         cy.getByData('search').click()
         cy.intercept('GET', '*').fixture('movie-response.json').as('movieResponse')
-        /*cy.wait('@movieResponse').then(interception => {
-            expect(interception.response?.statusCode).to.eq(200)
-        })*/
         cy.getByData('viewList').should('exist')
             .getByData('movieItem')
             .should('have.length', 2)
@@ -21,8 +18,6 @@ describe('index.tsx', () => {
 
     
     it('search movie is empty', () => {
-        /*cy.intercept('GET', '/reviews/search.json')
-            .fixture('empty-response.json', {timeout: 10000}).as('empty')*/
         cy.getByData('query').find('input').type('Super Marios')
         cy.getByData('search').click()
         cy.getByData('viewList').contains('No movies found')
@@ -39,13 +34,14 @@ describe('index.tsx', () => {
             .should('have.length.greaterThan', 20)
     })
 
-    it('open reviewer modal', () => {
+    it.only('open reviewer modal', () => {
         cy.getByData('query').find('input').type('Kill Bill')
         cy.getByData('search').click()
         cy.getByData('movieItem').eq(1).within($movieItem => {
             cy.wrap($movieItem).getByData('reviewer').find('span').click()
         })
         cy.getByData('modalReviewer').should('exist')
-        cy.getByData('')
+        cy.getByData('modalButton').click()
+        cy.getByData('modalReviewer').should('not.exist')
     })
 })
